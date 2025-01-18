7d3345107178deb2d8f2d12dadd0c268f1e3422b
KaifBhombal123 <99241435+Skaif-123@users.noreply.github.com>
2025-01-18 10:06:34 +0000

    all set to connect to spring boot backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4000, // Change to your desired port
+    host: true, // Allows access from external devices or tools
+  },
 })
